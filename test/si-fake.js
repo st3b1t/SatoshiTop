176a2989a4c1dfe9cc8fa9df93115544d63b96ff
st3b1t <st3b1t@pm.me>
2023-07-13 18:50:15 +0200

    multiple disk and layout

---
+++ test/si-fake.js
@@ -0,0 +1,30 @@
+
+//TODO convert in json rpc server
+
+const rnd = Math.random;
+
+let uptime = rnd()*1000000;
+
+module.exports = {
+    currentLoad: () => {
+        //TODO make random data
+    },
+    mem: () => {
+        //TODO make random data
+    }
+    fsSize: () => {
+        //TODO make random data
+    },
+    networkInterfaceDefault: () => {
+        //TODO make random data
+    },
+    networkStats: () => {
+        //TODO make random data
+    },
+    processes: () => {
+        //TODO make random data
+    },
+    cpuTemperature: () => {
+        //TODO make random data
+    },
+}
